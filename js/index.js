266a0e03c88b13018d54a150d3fa609e73bacb08
Biplob kafle <biplobkafle21@gmail.com>
2024-08-24 12:45:33 +0545

    navbar toggle done

--- js/index.js
+++ js/index.js
@@ -1,32 +1,15 @@
-const hamtoogle = document.querySelector(".ham-toogle");
-// const sidebardiv = document.querySelector(".sidebar-div");
-let visible = false
-hamtoogle.addEventListener("click", () => {
-    const sidebardiv = document.querySelector(".sidebar-div");
-    if (!visible) {
-        sidebardiv.style.visibility = "visible"
-        sidebardiv.style.transition = "all 0.35s cubic-bezier(0.6, -0.28, 0.74, 0.05) 0s"
-        sidebardiv.style.left = "0"
-        visible = true
-    }
-    else if (visible) {
-        sidebardiv.style.transition = "all 0.35s cubic-bezier(0.6, -0.28, 0.74, 0.05) 0s"
-        sidebardiv.style.left = "0%"
-        sidebardiv.style.visibility = "hidden"
-        visible = false
-    }
-})
-body.addEventListener("click", () => {
-    if (visible) {
-        sidebardiv.style.transition = "all 0.35s cubic-bezier(0.6, -0.28, 0.74, 0.05) 0s"
-        sidebardiv.style.left = "0%"
-        sidebardiv.style.visibility = "hidden"
-        visible = false
-    }
-})
+const hamburger = document.querySelector(".ham-toogle")
+const navlinks = document.querySelector(".nav-links")
+const navparts = document.querySelectorAll(".nav-links li a")
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle('active');
+    navlinks.classList.toggle("active")
 
-hamtoogle.addEventListener("click", function(){
-    sidebardiv.classList.toggle('show_sidebar');
-    console.log('don')
+})
+navparts.forEach((item) => {
+    item.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navlinks.classList.remove("active")
+    })
 
 })
